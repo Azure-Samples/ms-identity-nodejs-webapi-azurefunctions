b8a6ce891473df91396286c74c27da65d1a9c773
derisen <dogan.erisen@gmail.com>
2020-09-24 14:43:53 -0700

    project revised

--- Function/MyHttpTrigger/index.js
+++ Function/MyHttpTrigger/index.js
@@ -1,20 +1,19 @@
-const createHandler = require("azure-function-express").createHandler;
-const express = require("express");
+const express = require('express');
 const passport = require('passport');
+const auth = require('../auth.json');
 
-var BearerStrategy = require("passport-azure-ad").BearerStrategy;
+const createHandler = require('azure-function-express').createHandler;
 
-// Modify the below three lines to suit your environment
-var tenantID = "cbaf2168-de14-4c72-9d88-f5f05366dbef";
-var clientID = "e767d418-b80b-4568-9754-557f40697fc5";
-var appIdURI = "e767d418-b80b-4568-9754-557f40697fc5"; //"https://funcapi.<tenantname>.onmicrosoft.com";
+const BearerStrategy = require("passport-azure-ad").BearerStrategy;
 
-var options = {
-    identityMetadata: "https://login.microsoftonline.com/" + tenantID + "/v2.0/.well-known/openid-configuration",
-    clientID: clientID,
-    validateIssuer: false,
-    loggingLevel: "info",
-    passReqToCallback: false
+const options = {
+    identityMetadata: 'https://login.microsoftonline.com/' + auth.tenantID + '/v2.0/.well-known/openid-configuration',
+    clientID: auth.clientID,
+    audience: auth.audience,
+    validateIssuer: auth.validateIssuer,
+    passReqToCallback: auth.passReqToCallback,
+    loggingLevel: auth.loggingLevel,
+    scope: auth.scope
 };
 
 const bearerStrategy = new BearerStrategy(options, (token, done) => {
@@ -25,25 +24,40 @@ const bearerStrategy = new BearerStrategy(options, (token, done) => {
 const app = express();
 
 app.use(require('morgan')('combined'));
-app.use(require('body-parser').urlencoded({ "extended": true }));
+
+app.use(require('body-parser').urlencoded({ 'extended': true }));
+
 app.use(passport.initialize());
+
 passport.use(bearerStrategy);
 
-// Enable CORS (for testing only)
+// Enable CORS (for local testing only -remove in production/deployment)
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
-// This is where your API methods are exposed
-app.get("/api",
-    passport.authenticate("oauth-bearer", { session: false }),
-    function (req, res) {
-        var claims = req.authInfo;
-        console.log("Validated claims: ", JSON.stringify(claims));
-        console.log("body text: ", JSON.stringify(req.body));
-        res.status(200).json(claims);
+// Expose and protect API endpoint
+app.get('/api', passport.authenticate('oauth-bearer', { session: false }),
+    (req, res) => {
+
+        console.log('Validated claims: ', req.authInfo);
+
+        if (req.authInfo['scp'].split(' ').indexOf('demo.read') >= 0) {
+
+            // Service relies on the name claim.  
+            res.status(200).json({
+                'request-for': 'access_token',
+                'requested-by': req.authInfo['name'],
+                'issued-by': req.authInfo['iss'],
+                'issued-for': req.authInfo['aud'],
+                'scope': req.authInfo['scp']
+            });
+
+        } else {
+            res.status(403).json({'error': 'insufficient_scope'}); 
+        }
     }
 );
 
