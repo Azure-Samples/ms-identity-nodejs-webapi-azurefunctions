c6290c2cf1c023d0c67128a5393b0a9a7baa565b
derisen <dogan.erisen@gmail.com>
2020-09-25 13:55:43 -0700

    readme revised

--- Function/MyHttpTrigger/index.js
+++ Function/MyHttpTrigger/index.js
@@ -7,7 +7,8 @@ const createHandler = require('azure-function-express').createHandler;
 const BearerStrategy = require("passport-azure-ad").BearerStrategy;
 
 const options = {
-    identityMetadata: 'https://login.microsoftonline.com/' + auth.tenantID + '/v2.0/.well-known/openid-configuration',
+    identityMetadata: `https://login.microsoftonline.com/${auth.tenantID}/v2.0/.well-known/openid-configuration`,
+    issuer: `https://login.microsoftonline.com/${auth.tenantID}/v2.0`,
     clientID: auth.clientID,
     audience: auth.audience,
     validateIssuer: auth.validateIssuer,
@@ -41,23 +42,16 @@ app.use((req, res, next) => {
 // Expose and protect API endpoint
 app.get('/api', passport.authenticate('oauth-bearer', { session: false }),
     (req, res) => {
-
         console.log('Validated claims: ', req.authInfo);
 
-        if (req.authInfo['scp'].split(' ').indexOf('demo.read') >= 0) {
-
-            // Service relies on the name claim.  
-            res.status(200).json({
-                'request-for': 'access_token',
-                'requested-by': req.authInfo['name'],
-                'issued-by': req.authInfo['iss'],
-                'issued-for': req.authInfo['aud'],
-                'scope': req.authInfo['scp']
-            });
-
-        } else {
-            res.status(403).json({'error': 'insufficient_scope'}); 
-        }
+        // Service relies on the name claim.  
+        res.status(200).json({
+            'request-for': 'access_token',
+            'requested-by': req.authInfo['name'],
+            'issued-by': req.authInfo['iss'],
+            'issued-for': req.authInfo['aud'],
+            'scope': req.authInfo['scp']
+        });
     }
 );
 
